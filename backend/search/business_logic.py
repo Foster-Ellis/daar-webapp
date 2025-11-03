94044e2629305c57ed4497929b5ec40ff614198f
Graham Preston <graham.preston@gmail.com>
2025-11-03 12:38:34 +0100

    Index function
    Note that SearchIndex type is changed w.r.t spec, not updating spec quite yet just in case this changes again

---
+++ backend/search/business_logic.py
@@ -0,0 +1,47 @@
+import os
+
+from pathlib import Path
+from typing import Dict, Generator, TextIO
+
+Term = str # normalized: [a-zA-Z]
+DocumentId = int
+
+SearchIndex = Dict[Term, Dict[DocumentId, int]] # term => document id => occurrence count
+
+
+def _document_id_from_filename(fn: str) -> int:
+    [doc_id, _ext] = fn.split(".")
+    return int(doc_id)
+
+
+def _term_generator(f: TextIO) -> Generator[Term]:
+    i = 0
+    while i < 10:
+        yield "hi"
+
+
+def _indexable(term: str) -> bool:
+    return True
+
+
+def _increment_term_for_document(index: SearchIndex, term: Term, document_id: DocumentId) -> None:
+    if term not in index:
+        index[term] = dict()
+    if document_id not in index[term]:
+        index[term][document_id] = 0
+    index[term][document_id] += 1
+
+
+def index(documents_root: Path) -> SearchIndex:
+    index: SearchIndex = dict()
+
+    for fn in os.listdir(documents_root):
+        document_id = _document_id_from_filename(fn)
+
+        with open(fn) as f:
+            for term in _term_generator(f):
+                if _indexable(term):
+                    _increment_term_for_document(index, term, document_id)
+
+
+    return index
